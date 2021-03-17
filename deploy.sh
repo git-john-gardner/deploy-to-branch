e3551566d552fbeb9a380bbc0c875128f635666b
John Gardner <gardner.john.97+github@gmail.com>
2021-03-17 08:18:03 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,21 @@ build_command() {
 }
 
 echo -e "\033[0;32mDeleting old content...\033[0m"
-# rm -rf $directory
+rm -rf $directory
 
 echo -e "\033[0;32mChecking out $branch....\033[0m"
-# git worktree add $directory $branch
+git worktree add $directory $branch
 
 echo -e "\033[0;32mGenerating site...\033[0m"
 build_command
 
 latest_commit=$(git log '--format=format:%H' $master_name -1)
 
-# echo -e "\033[0;32mDeploying $branch branch...\033[0m"
-# cd $directory &&
-#   git add --all &&
-#   git commit -m "Build output as of $latest_commit" &&
-#   git push origin $branch
+echo -e "\033[0;32mDeploying $branch branch...\033[0m"
+cd $directory &&
+  git add --all &&
+  git commit -m "Build output as of $latest_commit" &&
+  git push origin $branch
 
 echo -e "\033[0;32mCleaning up...\033[0m"
-# git worktree remove $directory
+git worktree remove $directory
